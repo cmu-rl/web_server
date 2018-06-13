1b1bfe55973a47e720fd4682d11d1a187d6d3d6a
EC2 Default User <ec2-user@ip-172-31-18-104.us-east-2.compute.internal>
2018-06-13 02:19:39 +0000

    update status page

--- herobraine/sign_up/views.py
+++ herobraine/sign_up/views.py
@@ -2,28 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from .forms import UserForm
-#from django.contrib.auth import authenticate, login, logout
+#from .forms import UserForm
 from .server import add_user, get_status
 
 def form(request):
     if request.method == 'POST':
         # send this user name and password to user server
         u = request.POST.get('username', None)
-        p = request.POST.get('password', None)
-        print("username is:", u)
+        p = request.POST.get('email', None)
+        #print("username is:", u)
         add_user(u,p)
         return HttpResponseRedirect(reverse('sign_up:status', args=(u,)))
     else:
-        form = UserForm
-        return render(request, 'sign_up/form.html', {'form': form})
+        #form = UserForm
+        #return render(request, 'sign_up/form.html', {'form': form})
+        return render(request, 'sign_up/form.html')
 
 def status(request, username): 
-    #if request.method == 'POST':
     status = get_status(username)
-    #random = recv["queue_position"]
-    return render(request, 'sign_up/status.html',
-        {'username':username, 'queue_position':status["queue_position"]})
+    if status["banned"]:
+        return render(request, 'sign_up/banned.html')
+    else:  
+        return render(request, 'sign_up/status.html',
+            {'username':username, 'queue_position':status["queue_position"]})
 
 def index(request):
     return render(request, 'sign_up/index.html')
