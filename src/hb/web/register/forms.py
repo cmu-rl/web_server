04556e23ba098372472718d8ee5dc22e1c0f1a54
MirandaChen <yinglanc@andrew.cmu.edu>
2018-06-22 14:23:44 -0400

    add password min len

--- src/hb/web/register/forms.py
+++ src/hb/web/register/forms.py
@@ -4,7 +4,7 @@ class SignupForm(forms.Form):
     # max len of mc username is 16 characters
     username = forms.CharField(label="username", max_length=20) 
     email = forms.EmailField()
-    password = forms.CharField(label="password",
+    password = forms.CharField(min_length=6, label="password",
                                 widget=forms.PasswordInput)
-    repwd = forms.CharField(label="repwd",
+    repwd = forms.CharField(min_length=6, label="repwd",
                                 widget=forms.PasswordInput)
