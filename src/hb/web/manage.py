9838357ba262b58a039de9658f3c4b0c6fcb5b06
MirandaChen <yinglanc@andrew.cmu.edu>
2018-06-22 16:36:52 -0400

    sys append

--- src/hb/web/manage.py
+++ src/hb/web/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-
+sys.append("/home/ubuntu/web_server/src/")
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "herobraine.settings")
     try:
