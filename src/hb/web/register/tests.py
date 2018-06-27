a63778c58f170f672daf31caf162a7abc2c369a2
MirandaChen <yinglanc@andrew.cmu.edu>
2018-06-27 12:09:38 -0400

    add some basic automated testing

--- src/hb/web/register/tests.py
+++ src/hb/web/register/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from hb.user_server import add_user
-# Create your tests here.
+from django.urls import reverse
+
+class SignupTests(TestCase):
+    def test_signup_home(self):
+        response = self.client.get("/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_form(self):
+        response = self.client.get("/signup/form")
+        self.assertEqual(response.status_code, 200)
+    
+
+    def test_submission(self):
+        url = '/signup/form'
+        response = self.client.post(url, {'username':'randa'})
+        self.assertEqual(response.status_code, 200)
+        print(response.content)
